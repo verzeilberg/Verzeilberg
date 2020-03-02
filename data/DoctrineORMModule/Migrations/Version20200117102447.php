2738d70bf0a9efef71462a988d69618ef5d0bd01
Sander Verzeilberg <sander.verzeilberg@reports.nl>
2020-03-02 21:40:24 +0100

    WIP

---
+++ data/DoctrineORMModule/Migrations/Version20200117102447.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DoctrineORMModule\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20200117102447 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('CREATE TABLE token (userId INT NOT NULL, username VARCHAR(255) NOT NULL, IP VARCHAR(15) NOT NULL, sessionID VARCHAR(40) NOT NULL, timestamp INT NOT NULL, token VARCHAR(35) NOT NULL, PRIMARY KEY(userId)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('DROP TABLE token');
+    }
+}
